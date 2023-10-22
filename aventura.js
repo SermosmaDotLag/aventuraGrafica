80afff7b973a9249b65148f3568e078cc7d0269b
Mario <mariofuentesfernandez.guadalupe@alumnado.fundacionloyola.net>
2023-10-22 20:32:35 +0200

    mariosubioesto

--- aventura.js
+++ aventura.js
@@ -1,5 +1,28 @@
 console.log('Script cargado correctamente.')
-import {Escena} from './escena.js'
-let historia = new Escena
 
-historia.escena1()
+import { Escena } from './escena.js'
+
+let historia = new Escena()
+let siguiente = document.getElementById("siguiente")
+let opcion1 = document.getElementById("1")
+let escena = 1
+
+function recargarescenas(){
+    if(escena==1)
+    historia.escena1()
+    if(escena==2)
+    historia.escena2()
+}
+
+siguiente.onclick = function() {
+    historia.avanzar()
+    recargarescenas()
+}
+
+opcion1.onclick = function(){
+    opcion1.style.display = 'none'
+    escena=2
+    historia.reiniciarEscena()
+    historia.avanzar()
+    recargarescenas()
+}
--- escena.js
+++ escena.js
@@ -6,21 +6,49 @@ import {Enano} from './enano.js'
 
 export class Escena{
     #personaje1 = new Personaje('bbbb',Enano,Mago)
+    #lineaactual =0
     narrar(texto){
-        document.getElementById('divHistoria').innerHTML += '<b>Narrador: '+texto+'</b>'
+        document.getElementById('divHistoria').innerHTML = '<b>Narrador: '+texto+'</b>'
     }
-    limpiardiv(){
-        document.getElementById('divHistoria').innerHTML = ' '
+    introducirImagen(imagen){
+        document.getElementById('whosTalking').src='./imagenes/'+imagen
     }
-    introducirimagen(imagen){
-        //document.getElementById('divHistoria').innerHTML += '<img src=./imagenes/'+imagen+'>'
+    cambiarFondo(imagen) {
+        document.getElementById('fondo').style.backgroundImage="url('./imagenes/"+imagen+"')"
+    }
+    generarBoton(contenido,id){
+        let boton=document.getElementById(id)
+        boton.textContent = contenido
+        boton.style.display = "block"
+    }
+    avanzar(){
+        this.#lineaactual++
+    }
+    reiniciarEscena(){
+        this.#lineaactual=0
     }
     escena1(){
-        this.narrar('<i>Pipo el Gladiador era un guerrero de baja estatura que vivía en la mágica Ciudad de Eldoria, en el Reino de Arkania. A pesar de su pequeña estatura, tenía un corazón valiente y un espíritu indomable. Desde muy joven, Pipo soñaba con ser un gladiador, y a pesar de las burlas y el escepticismo de quienes lo rodeaban, se dedicó a entrenar incansablemente.')
-        this.introducirimagen("pipo.jpg")
+        switch(this.#lineaactual){
+            case 1:
+                this.narrar('<i>Pipo el Gladiador era un guerrero de baja estatura que vivía en la mágica Ciudad de Eldoria, en el Reino de Arkania. A pesar de su pequeña estatura, tenía un corazón valiente y un espíritu indomable. Desde muy joven, Pipo soñaba con ser un gladiador, y a pesar de las burlas y el escepticismo de quienes lo rodeaban, se dedicó a entrenar incansablemente.')
+                break;
+            case 2:
+                this.narrar('uno')
+                break;
+            default:
+                this.generarBoton('escena1','1')
+        }
     }
-    cambiarFondo() {
-        document.body.style.background = "url('bosque.jpg') no-repeat";
-        document.body.style.backgroundSize = "cover";
+    escena2(){
+        switch(this.#lineaactual){
+            case 1:
+                this.narrar('escena 2')
+                break;
+            case 2:
+                this.narrar('dos')
+                break;
+            default:
+                this.generarBoton('escena2','1')
+        }
     }
 }
